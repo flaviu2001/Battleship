cf190b5c1fc650e671345d46eb5d5ff3b7a49228
flaviu2001 <craciunflaviu1@gmail.com>
2020-01-02 01:55:45 +0200

    Small bugfix

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from ui import UI
 from gui import GUI
+from domain import Board
 from settings import Settings
 try:
     Settings().valid()
+    for _ in range(1000):
+        Board.random_ships()  # Checking for impossibilities regarding ship placements
     if Settings().ui() == "ui":
         UI().start()
     else:
         GUI().start()
 except Exception as e:
-    raise e
     print(e)
